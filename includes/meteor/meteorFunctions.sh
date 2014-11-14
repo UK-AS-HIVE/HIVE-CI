1b983e3e7f290f6520c2bf4ad5d8ce96b65a7e67
mpaddock <mpaddock@gmail.com>
2014-11-14 14:14:53 -0500

    Moved iOS functions into a separate file.

--- includes/meteor/meteorFunctions.sh
+++ includes/meteor/meteorFunctions.sh
@@ -34,35 +34,8 @@ function buildMeteor() {
   find . -name "index.html" -type f -print0 | xargs -0 gsed -i 's#"ROOT_URL_PATH_PREFIX":""#"ROOT_URL_PATH_PREFIX":"'"${REPO}"'"#g'
   find . -name "index.html" -type f -print0 | xargs -0 gsed -i 's#"DDP_DEFAULT_CONNECTION_URL":"'"${DEV_SERVER}"'"#"DDP_DEFAULT_CONNECTION_URL":"'"${DEV_SERVER}/${REPO}"'"#g'
 
-  if [[ -e build/ios/project ]]
-  then
-    cd build/ios/project
-
-    # Currently the onlyway to generate schemes, necessary for build, is to actually open XCode
-    open ${REPO}.xcodeproj &
-    echo "Waiting for XCode to generate scheme..."
-    while [[ ! -e ${REPO}.xcodeproj/project.xcworkspace/xcuserdata ]]
-    do
-      sleep 2
-    done
-    sleep 2
-    killall Xcode
-    wait $!
-    
-    #Change bundle identifier to match organization.
-    #TODO: Update version/build numbers as well?
-    PROJ_DIR=`pwd`
-    info_plist=$(ls ${REPO}/*Info.plist | sed -e 's/\.plist//')
-    defaults write ${PROJ_DIR}/${info_plist} CFBundleIdentifier ${ORG_PREFIX}.${REPO} 
-
-    xcodebuild archive -project ${REPO}.xcodeproj -scheme ${REPO} -archivePath ${REPO}.xcarchive
-    xcodebuild -exportArchive -archivePath ${REPO}.xcarchive -exportPath ${REPO} -exportFormat ipa -exportProvisioningProfile "HiveMobilePlatform InHouse ProvisioningProfile"
-    generateManifest
-    # TODO: this should be replace by the actual deployment
-    mkdir -p ${STAGE_DIR}/var/www
-    cp ${REPO}.ipa ${STAGE_DIR}/var/www/
-  fi
-
+  buildIos
+  
   if [[ -e ${BUILD_DIR}/${REPO}/build/android/project ]]
   then
     cd ${BUILD_DIR}/${REPO}/build/android/project
@@ -73,7 +46,6 @@ function buildMeteor() {
   
   BUILD_STATUS=$?
 
-  #TODO deploy using rsync, etc.
 }
 
 # Execute from within the main directory before building
@@ -103,43 +75,3 @@ function deployMeteor() {
 
 }
 
-function generateManifest() {
-  PROJ_DIR=`pwd`
-  info_plist=$(ls ${REPO}/*Info.plist | sed -e 's/\.plist//')
-  bundle_id=$(defaults read $PROJ_DIR/$info_plist CFBundleIdentifier)
-  cat << EOF > ${REPO}.plist
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-  <dict>
-    <key>items</key>
-    <array>
-      <dict>
-        <key>assets</key>
-        <array>
-          <dict>
-            <key>kind</key>
-            <string>software-package</string>
-            <key>url</key>
-            <string>${DEV_SERVER}/${REPO}.ipa</string>
-          </dict>
-        </array>
-        <key>metadata</key>
-        <dict>
-          <key>bundle-identifier</key>
-          <string>$bundle_id</string>
-          <key>kind</key>
-          <string>software</string>
-          <key>title</key>
-          <string>${REPO}</string>
-        </dict>
-      </dict>
-    </array>
-  </dict>
-</plist>
-EOF
-
-#TODO: Deploy with the .ipa
-mkdir -p ${STAGE_DIR}/var/www
-cp ${REPO}.plist ${STAGE_DIR}/var/www/
-} 
